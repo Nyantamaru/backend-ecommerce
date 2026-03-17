ca9f18215901db753fe49d289710ee299e6cefb1
manuel <manekete@gamail.com>
2026-03-17 16:14:21 -0600

    first commit init spring boot project

---
+++ src/main/java/com/manuel/scommerce/SpringbBootEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.manuel.scommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbBootEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbBootEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manuel/scommerce/SpringbBootEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manuel.scommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbBootEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
